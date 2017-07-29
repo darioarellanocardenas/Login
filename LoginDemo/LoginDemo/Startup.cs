364705cb141d4c8b864af827cf3de9a492a1c9ec
darioarellanocardenas <darioarellano2003@gmail.com>
2017-07-29 10:20:20 -0500

    Creacio de proyecto y entity
    Creacion

---
+++ LoginDemo/LoginDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LoginDemo.Startup))]
+namespace LoginDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
